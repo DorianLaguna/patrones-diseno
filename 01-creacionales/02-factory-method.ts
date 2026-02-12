81ccef5814163cfbfffe24427aa656c14b64067c
Dorian Laguna <richardlgcs@gmail.com>
2026-02-12 15:06:46 -0600

    learn concept about factory pattern

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,78 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { error } from "node:console";
+import { COLORS } from "../helpers/colors.ts";
+
+
+interface Hamburguer {
+    prepare(): void;   
+}
+
+class ChickenHamburguer implements Hamburguer {
+    prepare(): void {
+      console.log('preparando hamburguesa de pollo', COLORS.yellow)
+    }
+}
+class BeefHamburguer implements Hamburguer {
+    prepare(): void {
+      console.log('preparando hamburguesa de res', COLORS.yellow)
+    }
+}
+
+class BeanHamburguer implements Hamburguer {
+  prepare(): void {
+    console.log('preparando hamburguesa de frijoles, que asco', COLORS.cyan)
+  }
+}
+
+abstract class Restaurant {
+    abstract createHamburguer(): Hamburguer;
+
+    orderHamburguer(): void {
+        const hamburguer = this.createHamburguer();
+        hamburguer.prepare();
+    }
+}
+
+class BeanRestaurant extends Restaurant {
+  override createHamburguer(): Hamburguer {
+    return new BeanHamburguer()
+  }
+
+}
+
+class ChickeRestaurant extends Restaurant {
+    override createHamburguer(): Hamburguer {
+      return new ChickenHamburguer()
+    }
+}
+class BeefRestaurant extends Restaurant {
+    override createHamburguer(): Hamburguer {
+      return new BeefHamburguer()
+    }
+}
+
+function main(){
+
+    let restaurant: Restaurant;
+    const burgerType = prompt('Que hamburguesa quieres? ( chiken/beef/bean )')
+    switch(burgerType){
+        case 'chicken':
+            restaurant = new ChickeRestaurant()
+            break;
+        case 'beef':
+            restaurant = new BeefRestaurant();
+            break;
+        case 'bean':
+            restaurant = new BeanRestaurant();
+            break;
+        default:
+            throw new Error('no existe ninguna hamburguesa de ese tipo')
+    }
+
+    restaurant.orderHamburguer();
+}
+
+main()
